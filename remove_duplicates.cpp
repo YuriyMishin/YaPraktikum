d764c19507fd6fcfe0f9d4a7c71fd55c30239d80
Yuriy Mishin <86564294+YuriyMishin@users.noreply.github.com>
2021-08-25 17:31:25 +0300

    Update remove_duplicates.cpp

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -1,36 +1,24 @@
 #include "remove_duplicates.h"
 
 void RemoveDuplicates(SearchServer& search_server){
-    std::vector<std::map <std::string, double>> all_uniq_documents;
+    std::set<std::set <std::string>> all_uniq_documents;
     std::vector<int> duplicated_documents_id;
-    bool is_duplicate;
+    std::set<std::string> next_document_data;
     
     for (const int document_id : search_server){
-        if (all_uniq_documents.size() == 0) {
-            all_uniq_documents.push_back(search_server.GetWordFrequencies(document_id));
+        next_document_data = {};
+        for (const auto& [word, _] : search_server.GetWordFrequencies(document_id)){
+            next_document_data.insert(word);
+        }
+        if (all_uniq_documents.emplace(next_document_data).second) {
             continue;
         }
-        auto next_document_data = search_server.GetWordFrequencies(document_id);
-        is_duplicate = false;
-        for (int document_num = 0; document_num < all_uniq_documents.size(); ++document_num){
-            const auto& document_data = all_uniq_documents.at(document_num);
-            
-            if ((document_data.size() == next_document_data.size()) && std::equal(document_data.begin(), document_data.end(), next_document_data.begin(), [](const auto& lhs, const auto& rhs) {
-                return lhs.first == rhs.first;})) {
-                    
-                    duplicated_documents_id.push_back(document_id);
-                    is_duplicate = true;
-                    break;
-                }
-        }
-        if (!is_duplicate) {
-            all_uniq_documents.push_back(next_document_data);
-        }
+        duplicated_documents_id.push_back(document_id);
+        std::cout << "Found duplicate document id "<< document_id << std::endl;
     }
-    
     for (int duplicated_id : duplicated_documents_id)
     {
-        std::cout << "Found duplicate document id "<< duplicated_id << std::endl;
         search_server.RemoveDocument(duplicated_id);
     }
 }
+
