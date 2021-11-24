663dc388b0ca2021dbb4a14930bbdfc1cd357fcd
Yuriy Mishin <86564294+YuriyMishin@users.noreply.github.com>
2021-11-24 16:41:28 +0300

    Update geo.h

--- Sprint_9/geo.h
+++ Sprint_9/geo.h
@@ -12,8 +12,9 @@ namespace tc::detail{
     inline double ComputeDistance(Coordinates from, Coordinates to) {
         using namespace std;
         static const double dr = 3.1415926535 / 180.;
+        const double r_earth =  6371000.;
         return acos(sin(from.lat * dr) * sin(to.lat * dr)
                     + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-               * 6371000;
+               * r_earth;
     }
 }
