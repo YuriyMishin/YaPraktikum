93bf7df677375fda8489d8f02f2f82716978f790
Yuriy Mishin <86564294+YuriyMishin@users.noreply.github.com>
2021-11-24 16:40:03 +0300

    Update input_reader.h

--- Sprint_9/input_reader.h
+++ Sprint_9/input_reader.h
@@ -8,9 +8,10 @@
 namespace tc::detail {
     void TrimStr(std::string &str);
     std::pair<int, std::string> ParseDescriptionDistance(std::istream& ist) ;
+    std::string ParseNameFromQuery(std::string& query);
+    std::vector <std::string> ParseDescriptionRoute (std::string &query_description_route);
     Stop ParseQueryStop (std::string& query, TransportCatalogue& transport_catalogue);
     Bus ParseQueryBus (std::string& query, TransportCatalogue& transport_catalogue);
 }
 
 std::istream& operator >> (std::istream& input, tc::TransportCatalogue& transport_catalogue);
-
