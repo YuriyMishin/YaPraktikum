3bdd9a3bacf76ea724e5e99337bc52c34a4db9ea
Yuriy Mishin <86564294+YuriyMishin@users.noreply.github.com>
2021-11-24 17:08:55 +0300

    Update stat_reader.h

--- Sprint_9/stat_reader.h
+++ Sprint_9/stat_reader.h
@@ -5,7 +5,7 @@
 #include "transport_catalogue.h"
 #include "input_reader.h"
 namespace tc::detail {
-    void AnswerBus(std::ostream &output, std::string &bus_name, tc::TransportCatalogue &transport_catalogue);
-    void AnswerStop(std::ostream &output, std::string &stop_name, tc::TransportCatalogue &transport_catalogue);
+    void PrintBusInfo(std::ostream &output, std::string &bus_name, tc::TransportCatalogue &transport_catalogue);
+    void PrintStopInfo(std::ostream &output, std::string &stop_name, tc::TransportCatalogue &transport_catalogue);
 }
 std::ostream& operator << (std::ostream& output, tc::TransportCatalogue& transport_catalogue);
